90738fd5d31cbedaf670dd0a25ab4c51a8809fb9
mjannepally <mjannepally@ancestry.com>
2014-07-18 11:22:14 -0700

    test

---
+++ test/usagetest.js
@@ -0,0 +1,13 @@
+var request = require('supertest'),
+    express = require('express'),
+    expect = require('chai').expect,
+    assert = require('chai').assert,
+    sinon = require('sinon'),
+    http = require('http');
+var usage = require('../lib/usage');
+
+describe('Usage tests', function(){
+    it('Get stats', function(done) {
+
+    });
+});
